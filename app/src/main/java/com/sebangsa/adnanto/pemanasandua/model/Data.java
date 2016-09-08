2117609a08f542ed19bf841c8a1e2488e113bc43
adahra <adnantoa@gmail.com>
2016-09-08 18:24:59 +0700

    Perubahan pada kelas Data

--- app/src/main/java/com/sebangsa/adnanto/pemanasandua/model/Data.java
+++ app/src/main/java/com/sebangsa/adnanto/pemanasandua/model/Data.java
@@ -6,9 +6,6 @@ import com.google.gson.annotations.SerializedName;
 import java.util.ArrayList;
 import java.util.List;
 
-import io.realm.RealmList;
-import io.realm.RealmObject;
-
 /**
  * Created by adnanto on 8/31/16.
  */
