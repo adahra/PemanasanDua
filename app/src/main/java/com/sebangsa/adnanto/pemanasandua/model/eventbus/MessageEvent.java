badef24541788c1fcbaadc81e8f1bb5ffa9404ce
adahra <adnantoa@gmail.com>
2016-09-15 12:51:50 +0700

    Perubahan pada kelas eventbus handler

---
+++ app/src/main/java/com/sebangsa/adnanto/pemanasandua/model/eventbus/MessageEvent.java
@@ -0,0 +1,12 @@
+package com.sebangsa.adnanto.pemanasandua.model.eventbus;
+
+/**
+ * Created by adnanto on 9/14/16.
+ */
+public class MessageEvent {
+    private final String message;
+
+    public MessageEvent(String message) {
+        this.message = message;
+    }
+}
